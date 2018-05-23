4062782845a98cc8939c718369e55d7f537853d2
Momin Khan <foolmoron@gmail.com>
2018-05-23 03:13:10 -0400

    Basic leaderboard animation

---
+++ Assets/Leaderboard.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class LeaderboardEntry {
+	static int ID;
+	public readonly int Id = ID++;
+
+	public string Name;
+	public int Points;
+	public int CreatedTime;
+}
+[Serializable]
+public class LeaderboardPosition {
+	public int TopOffset;
+	public float Scale;
+}
+public class Leaderboard : MonoBehaviour {
+	
+	public GameObject EntryPrefab;
+
+	public LeaderboardPosition[] Positions;
+	public float DefaultScale;
+	public float DefaultOffsetStep;
+
+	[Range(0, 0.5f)]
+	public float LerpSpeed = 0.1f;
+
+	public List<LeaderboardEntry> Entries = new List<LeaderboardEntry>();
+	ListDict<LeaderboardEntry, Text> entryToTexts = new ListDict<LeaderboardEntry, Text>();
+	
+	void Update() {
+		// sort entries
+		Entries.Sort((e1, e2) => (e2.Points - e1.Points) != 0 ? e2.Points - e1.Points : (e1.CreatedTime - e2.CreatedTime) != 0 ? e1.CreatedTime - e2.CreatedTime : e1.Id - e2.Id);
+		// clear texts with old entries
+		for (int i = 0; i < entryToTexts.Count; i++) {
+			if (!Entries.Contains(entryToTexts.Keys[i])) {
+				entryToTexts.RemoveAt(i);
+				i--;
+			}
+		}
+		// new texts for new entries
+		for (var i = 0; i < Entries.Count; i++) {
+			var entry = Entries[i];
+			if (!entryToTexts.ContainsKey(entry)) {
+				var newText = Instantiate(EntryPrefab, transform).GetComponent<Text>();
+				entryToTexts[entry] = newText;
+				LerpToPosition(newText, i, 1);
+			}
+		}
+		// update leaderboard
+		for (int i = 0; i < Entries.Count; i++) {
+			var entry = Entries[i];
+			var text = entryToTexts[entry];
+			text.text = entry.Points + "☆ " + entry.Name;
+			LerpToPosition(text, i, LerpSpeed);
+		}
+	}
+
+	void LerpToPosition(Text text, int position, float speed) {
+		var desiredOffset = position < Positions.Length ? Positions[position].TopOffset : Positions[Positions.Length - 1].TopOffset + DefaultOffsetStep * (position - Positions.Length + 1);
+		var desiredScale = position < Positions.Length ? Positions[position].Scale : DefaultScale;
+		text.rectTransform.anchoredPosition = text.rectTransform.anchoredPosition.withY(Mathf.Lerp(text.rectTransform.anchoredPosition.y, desiredOffset, speed));
+		text.rectTransform.localScale = Vector3.Lerp(text.rectTransform.localScale, Vector3.one * desiredScale, speed);
+	}
+}
---
+++ Assets/SwapMaterialOnStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwapMaterialOnStart : MonoBehaviour {
+
+	public Material Material;
+	
+	void Start() {
+		if (GetComponent<Renderer>()) {
+			GetComponent<Renderer>().material = Material;
+		}
+		if (GetComponent<Graphic>()) {
+			GetComponent<Graphic>().material = Material;
+		}
+		enabled = false;
+	}
+}
